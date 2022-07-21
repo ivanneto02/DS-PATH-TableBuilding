fff17120f72ed3d8326dbbc6b9854bf26e29cad7
ivanneto02 <ivan.a.neto@hotmail.com>
2022-07-21 04:57:12 -0700

    main now calls other more encompassing modules.
    infer_relations.py
    	- Creates the data tables of relationships
    visualize_relations.py
    	- Visualizes the data tables of relationships

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ load_dotenv()
 
 PATH_TO_DATA = os.environ["PATH_TO_DATA"]
 DATA_FILE = os.environ["DATA_FILE"]
+N_ROWS = int(os.environ["N_ROWS"]) if os.environ["N_ROWS"] != "None" else None
--- main.py
+++ main.py
@@ -1,23 +1,7 @@
-# Import our own modules
-import src.RelRowStrategy as StrategyModule
-import src.RelationsTableBuilder as TableModule
-
-# Import libraries
-import pandas as pd
-
-from config import *
+import visualize_relations as viz
 
 def main():
-    print("> Starting")
-    print("> Reading data")
-    df = pd.read_csv(PATH_TO_DATA + "/" + DATA_FILE)
-    slices = ["mayoclinic"]
-
-    print("> Slicing based on sources")
-    df = df[ (df["source_name"] in slices) & (df["concept_type"] == "drug") ]
-
-    print(df["source_name"].value_counts())
-    pass
+    viz.visualize_relations()
 
 if __name__ == "__main__":
     main()
